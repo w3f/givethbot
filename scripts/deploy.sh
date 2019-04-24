244b607a0b417c3c0c58435fe19f8da6569905f9
Federico Gimenez <federico.gimenez@gmail.com>
2019-04-24 16:10:20 +0200

    add values to deployment command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,8 @@
 
 /scripts/deploy.sh helm \
                    --set image.tag="${CIRCLE_TAG}" \
+                   --set botCredentials="${BOT_CREDENTIALS}" \
+                   --set clientSecret="${CLEINT_SECRET}" \
+                   --set credentials="${CREDENTIALS}" \
                    givethbot \
                    w3f/givethbot
