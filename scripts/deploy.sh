2e37b50ba27d5929351efc2e778b18b95116c64d
Federico Gimenez <federico.gimenez@gmail.com>
2019-04-25 09:56:26 +0200

    removed unused messages and updated room id

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 /scripts/deploy.sh helm \
                    --set image.tag="${CIRCLE_TAG}" \
                    --set botCredentials="${BOT_CREDENTIALS}" \
-                   --set clientSecret="${CLEINT_SECRET}" \
+                   --set clientSecret="${CLIENT_SECRET}" \
                    --set credentials="${CREDENTIALS}" \
                    givethbot \
                    w3f/givethbot
