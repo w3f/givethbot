97c91999483eac17b1c70290e150df64eeb3ac3b
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-16 10:23:58 +0200

    update deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-/scripts/deploy.sh helm \
-                   --set image.tag="${CIRCLE_TAG}" \
-                   --set botCredentials="${BOT_CREDENTIALS}" \
-                   --set clientSecret="${CLIENT_SECRET}" \
-                   --set credentials="${CREDENTIALS}" \
-                   --set mainRoom="${MAIN_ROOM}" \
-                   givethbot \
-                   w3f/givethbot
+/scripts/deploy.sh -c ${ENVIRONMENT} -t helm -a "--set image.tag=${CIRCLE_TAG} --set botCredentials=${BOT_CREDENTIALS} --set clientSecret=${CLIENT_SECRET} --set credentials=${CREDENTIALS} --set mainRoom=${MAIN_ROOM} givethbot w3f/givethbot"
